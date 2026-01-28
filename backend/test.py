ed13191e43d233795883d04094af07d932c6d14b
Mirza Ilhami <mr.ilhami@gmail.com>
2026-01-28 18:44:42 +0700

    Force re-scan

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,11 @@
 import os
 import pickle
 
-def InvalidFunction(user_input):
+def InvalidFunction(user_input): # bad naming
     api_key = "sk-12345-supersecret"           # secrets regex
     query = f"SELECT * FROM users WHERE id = {user_input}"  # sql_injection
     os.system(query)                            # unsafe_exec regex
     exec(user_input)                            # unsafe_exec AST
     pickle.loads(user_input.encode())           # insecure_deserial AST
     print(query)                                # logging missing
+    print("No logging here")
